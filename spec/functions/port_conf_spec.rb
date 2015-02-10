77b2ebfd3f061ed5bdbe8c7029d7e6d3b0513890
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-02-09 20:53:37 -0500

    test producing a hash compatible with ports.conf config * with a single interface string listed * or  a list of ports with a mix of a port range and single interface

--- spec/functions/port_conf_spec.rb
+++ spec/functions/port_conf_spec.rb
@@ -2,7 +2,17 @@ require 'spec_helper'
 require 'pry-debugger'
 
 describe 'ports_conf' do
-  describe 'test mix of ranges and single interfaces' do
+  describe 'single interface' do
+    let (:myhash) { { '40G' => 'swp1' }}
+    it do
+      should run.with_params(myhash).and_return(
+        [
+          [1, "40G"]
+      ])
+    end
+  end
+
+  describe 'mix of ranges and single interfaces' do
     let(:myhash) { {
       '40G' => ['swp1-3'],
       '10G' => ['swp10', 'swp12-13']
